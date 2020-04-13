a69e28cc30b14cb60280733b94db6348cf27df8a
Ocean Hart <pezechigozirim@gmail.com>
2020-04-13 07:22:58 +0100

    Minor tweak

--- src/estimator.php
+++ src/estimator.php
@@ -1,15 +1,9 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With");
 
 
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    $inputData = json_decode(file_get_contents("php://input"));
-    echo covid19ImpactEstimator($inputData);
-}
+$inputData = json_decode($inputData);
+echo covid19ImpactEstimator($inputData);
 
 
 function covid19ImpactEstimator($data)
